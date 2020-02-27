bd1d072956279b52af67b926b0268ba458236d60
James Balamuta <balamut2@illinois.edu>
2020-02-26 21:31:26 -0600

    Merge best_model into model-selection

--- R/model-selection-criteria.R
+++ R/model-selection-criteria.R
@@ -1,3 +1,38 @@
+
+## Model selection ----
+
+#' Extract the Best Model
+#'
+#' Extracts the best model from the `auto_*()` search procedure.
+#'
+#' @param x   An `auto_edina` object
+#' @param ic  Selection criteria
+#' @param ... Not used.
+#'
+#' @return
+#' An `edina` object
+#'
+#' @export
+#' @rdname best_model
+best_model = function(x, ...) {
+    UseMethod("best_model")
+}
+
+#' @export
+#' @rdname best_model
+best_model.auto_edina = function(x, ic = c("ppp", "bic", "dic"), ...) {
+    ic = tolower(ic)
+    ic = match.arg(ic)
+
+    x$edina_models[[which.min(x$criterion[,ic])]]
+}
+
+#' @export
+best_model.default = function(x, ...) {
+    stop_bad_class(x, "auto_edina")
+}
+
+
 ### PPP Criteria ----
 
 #' Model Heuristic used for Model Selection
