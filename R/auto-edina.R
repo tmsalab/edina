805833226b1c3cabe0356ddf6ce519e5116a5a4f
James Balamuta <balamut2@illinois.edu>
2020-02-26 20:59:14 -0600

    Further refine the auto_edina object
    Drop save results.

--- R/auto-edina.R
+++ R/auto-edina.R
@@ -8,29 +8,53 @@
 #' @param k             Number of Attribute Levels as a positive `integer`.
 #' @param burnin        Number of Observations to discard on the chain.
 #' @param chain_length  Length of the MCMC chain
-#' @param save_results  Output intermediary estimations to R binary.
-#'                      Default: `FALSE`
-#' @param save_filename Name stem to save each `k` iteration to. Relevant if
-#'                      `save_results` is enabled.
-#' @return An `auto_edina` object
+#'
+#' @return
+#' An `auto_edina` object.
+#'
 #' @export
 #' @examples
+#' if(requireNamespace("simcdm", quietly = TRUE)) {
+#'
+#' # Set a seed for reproducibility
+#' set.seed(1512)
+#'
+#' # Setup data simulation parameters
+#' N = 15   # Number of Examinees / Subjects
+#' J = 10   # Number of Items
+#' K = 2    # Number of Skills / Attributes
+#'
+#' # Note:
+#' # Sample size and attributes have been reduced to create a minimally
+#' # viable example that can be run during CRAN's automatic check.
+#' # Please make sure to have a larger sample size...
+#'
+#' # Assign slipping and guessing values for each item
+#' ss = gs = rep(.2, J)
+#'
+#' # Simulate an identifiable Q matrix
+#' Q = simcdm::sim_q_matrix(J, K)
+#'
+#' # Simulate subject attributes
+#' subject_alphas = simcdm::sim_subject_attributes(N, K)
+#'
+#' # Simulate items under the DINA model
+#' items_dina = simcdm::sim_dina_items(subject_alphas, Q, ss, gs)
+#'
 #' \dontrun{
-#' library("tmsadata")
+#' # Requires at least 15 seconds of execution time.
+#' # Three EDINA models will be fit with increasing number of attributes.
+#' model_set_edina = auto_edina(items_dina, k = 2:4)
 #'
-#' # Load data
-#' data("trial_matrix", package="tmsadata")
+#' # Display results
+#' edina_model
 #'
-#' # Coerce to matrix
-#' trial_matrix = as.matrix(trial_matrix)
-#'
-#' edina_models = auto_edina(trial_matrix, k = 1:2)
+#' table = summary(edina_model)
 #' }
+#' }
+#'
 auto_edina = function(data, k = 2:4,
-                      burnin = 10000, chain_length = 20000,
-                      save_results = FALSE, save_filename = "edina_model_data") {
-
-    stopifnot(is.logical(save_results))
+                      burnin = 10000, chain_length = 20000) {
 
     ## Note:
     # Chain length is adjusted in edina
@@ -41,19 +65,11 @@ auto_edina = function(data, k = 2:4,
 
     message("Starting the estimation procedure ... ")
 
-    if(save_results) {
-        # Get the length of the string e.g. 200 => 3
-        nlen = max(nchar(k))
-
-        # Format string
-        save_file_ids = sprintf(paste0("%0",nlen,"d"), k)
-    }
-
     # Setup storage for EDINA Object
     outobj = vector('list', num_k)
 
     criterions = matrix(NA, nrow = num_k, ncol = 4)
-    colnames(criterions) = c("k", "bic", "dic", "heuristic")
+    colnames(criterions) = c("k", "bic", "dic", "ppp")
 
     for(i in seq_along(k)) {
         k_idx = k[i]
@@ -72,12 +88,6 @@ auto_edina = function(data, k = 2:4,
 
         message("Time Elapsed: ",  outobj[[i]][["timing"]][3])
 
-        if(save_results) {
-            # Create a single edina object
-            ## consider doing an `assign()` to match the filename
-            edina_obj = outobj[[i]]
-            save(edina_obj, file = paste0(save_filename, "_", save_file_ids[i],".rda"))
-        }
     }
 
     # Output all EDINA objects
@@ -90,7 +100,7 @@ auto_edina = function(data, k = 2:4,
 }
 
 
-#' Print method for `auto_dina`
+#' Print method for `auto_edina`
 #'
 #' Custom print method for displaying the results of the Auto EDINA method.
 #'
@@ -103,13 +113,19 @@ print.auto_edina = function(x, ...) {
     print(as.data.frame(x$criterions), digits = 4, row.names = FALSE, ...)
 }
 
+#' Summarize `auto_edina` model data
 #'
-#' Displays information about the value of each model information criterion
-#' for a given model across the dimensions the Q matrix is estimated.
 #' Custom summarization method for displaying the results of the `auto_edina`.
 #'
+#' @param x   An `auto_edina` object
+#' @param ... Additional methods passed onto the `print.data.frame` method.
 #'
+#' @return
+#' An invisible `data.frame` containing criterion information.
 #'
 #' @export
+summary.auto_edina = function(object, ...) {
+    print(object, ...)
+    invisible(as.data.frame(x$criterions))
 }
 
